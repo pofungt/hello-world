ce5f5c753d06a08b70fb90ea79b64282c8e3c77b
Duncan Tang <pofungt@hotmail.com>
2023-05-22 11:49:06 -0400

    removed map from main.js

--- src/main.js
+++ src/main.js
@@ -1,16 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-let map;
-
-async function initMap() {
-  const { Map } = await google.maps.importLibrary("maps");
-  map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
-}
-
-initMap();
-
 createApp(App).mount('#app')
