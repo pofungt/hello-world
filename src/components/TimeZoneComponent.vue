c8a464af8db22f54f2e5d1cd41f78ff8da5b474e
Duncan Tang <pofungt@hotmail.com>
2023-05-22 19:57:03 -0400

    Done local time clock of last searched timezone

--- src/components/TimeZoneComponent.vue
+++ src/components/TimeZoneComponent.vue
@@ -1,11 +1,38 @@
 <template>
-    <div>{{lastLocation.timeZoneId}} ({{lastLocation.timeZoneName}})</div>
+  <div>{{ lastLocation.timeZoneId }} ({{ lastLocation.timeZoneName }})</div>
+  <div>
+    <span id="span">{{ currentTime }}</span>
+  </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      currentTime: null,
+      intervalId: null
+    };
+  },
   props: {
-    lastLocation: Object
+    lastLocation: Object,
+  },
+  mounted() {
+    this.updateTime();
+    this.intervalId = setInterval(() => this.updateTime(), 1000);
+  },
+  methods: {
+    updateTime() {
+      this.currentTime = this.getLocalTime(this.lastLocation.timeZoneId);
+    },
+    getLocalTime(timeZoneId) {
+        const date = new Date();
+        const options = {timeZone: timeZoneId, hour: '2-digit', minute: '2-digit', second: '2-digit'};
+        const localTime = date.toLocaleString('en-US', options);
+        return localTime;
+    }
+  },
+  beforeUnmount() {
+    clearInterval(this.intervalId);
   },
 };
 </script>
