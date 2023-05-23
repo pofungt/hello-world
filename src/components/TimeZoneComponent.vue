fab1325743a7c729ccd3cec27d3604bbb17bf9fe
Duncan Tang <pofungt@hotmail.com>
2023-05-22 20:26:34 -0400

    changed to setup() for TimeZoneComponent

--- src/components/TimeZoneComponent.vue
+++ src/components/TimeZoneComponent.vue
@@ -1,4 +1,7 @@
 <template>
+  <div>
+    {{ lastLocation.address }}
+  </div>
   <div>{{ lastLocation.timeZoneId }} ({{ lastLocation.timeZoneName }})</div>
   <div>
     <span id="span">{{ currentTime }}</span>
@@ -6,33 +9,39 @@
 </template>
 
 <script>
+import { ref, onBeforeUnmount } from "vue";
+
 export default {
-  data() {
-    return {
-      currentTime: null,
-      intervalId: null
-    };
-  },
   props: {
     lastLocation: Object,
   },
-  mounted() {
-    this.updateTime();
-    this.intervalId = setInterval(() => this.updateTime(), 1000);
-  },
-  methods: {
-    updateTime() {
-      this.currentTime = this.getLocalTime(this.lastLocation.timeZoneId);
-    },
-    getLocalTime(timeZoneId) {
-        const date = new Date();
-        const options = {timeZone: timeZoneId, hour: '2-digit', minute: '2-digit', second: '2-digit'};
-        const localTime = date.toLocaleString('en-US', options);
-        return localTime;
+  setup(props) {
+    let currentTime = ref("");
+
+    function updateTime() {
+      currentTime.value = getLocalTime(props.lastLocation.timeZoneId);
     }
-  },
-  beforeUnmount() {
-    clearInterval(this.intervalId);
+
+    function getLocalTime(timeZoneId) {
+      const date = new Date();
+      const options = {
+        timeZone: timeZoneId,
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      };
+      const localTime = date.toLocaleString("en-US", options);
+      return localTime;
+    }
+
+    updateTime();
+    const intervalId = setInterval(() => updateTime(), 1000);
+
+    onBeforeUnmount(() => {
+      clearInterval(intervalId);
+    });
+
+    return { currentTime };
   },
 };
 </script>
